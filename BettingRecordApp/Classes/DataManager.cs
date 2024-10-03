8c58086d477499cc7b1957d876f9f7a7350df103
Luka Grubić <luka.grubic.ck@gmail.com>
2024-10-03 21:08:22 +0200

    Added DataManager class for handling betting record and recommendations data; Added UtilitySorting

---
+++ BettingRecordApp/Classes/DataManager.cs
@@ -0,0 +1,67 @@
+﻿namespace BettingRecordApp
+{
+    public static class DataManager
+    {
+        public static BettingRecord BettingRecord { get; set; }
+        public static Recommendations Recommendations { get; set; }
+
+        public static void Init()
+        {
+            BettingRecord = new BettingRecord();
+            Recommendations = new Recommendations();
+        }
+
+        public static void Init(BettingRecord _bettingRecord)
+        {
+            Init();
+
+            if (_bettingRecord != null)
+            {
+                BettingRecord = _bettingRecord;
+                Recommendations.LoadBettingRecordRecommendations(_bettingRecord);
+            }
+        }
+
+        public static void AddBetslip(Betslip _betslip)
+        {
+            BettingRecord.Betslips.Add(_betslip);
+            UtilitySorting.SortBetslipListByDate(BettingRecord.Betslips);
+
+            Recommendations.Update(_betslip);
+        }
+
+        public static void EditBetslip(int _betslipIndex, Betslip _betslip)
+        {
+            BettingRecord.Betslips[_betslipIndex] = _betslip;
+            UtilitySorting.SortBetslipListByDate(BettingRecord.Betslips);
+
+            Recommendations.Update(_betslip);
+        }
+
+        public static void RemoveBetslip(int _betslipIndex)
+        {
+            BettingRecord.Betslips.RemoveAt(_betslipIndex);
+        }
+
+        public static void AddEvent(int _betslipIndex, Event _event)
+        {
+            BettingRecord.Betslips[_betslipIndex].Events.Add(_event);
+            UtilitySorting.SortEventListByDateTime(BettingRecord.Betslips[_betslipIndex].Events);
+
+            Recommendations.Update(_event);
+        }
+
+        public static void EditEvent(int _betslipIndex, int _eventIndex, Event _event)
+        {
+            BettingRecord.Betslips[_betslipIndex].Events[_eventIndex] = _event;
+            UtilitySorting.SortEventListByDateTime(BettingRecord.Betslips[_betslipIndex].Events);
+
+            Recommendations.Update(_event);
+        }
+
+        public static void RemoveEvent(int _betslipIndex, int _eventIndex)
+        {
+            BettingRecord.Betslips[_betslipIndex].Events.RemoveAt(_eventIndex);
+        }
+    }
+}
---
+++ BettingRecordApp/Utility/UtilitySorting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+
+
+namespace BettingRecordApp
+{
+    public static class UtilitySorting
+    {
+        // --- Event sorting ---
+
+        public static void SortEventListByDateTime(List<Event> _eventList)
+        {
+            _eventList.Sort((e1, e2) => e1.EventInfo.DateTime.CompareTo(e2.EventInfo.DateTime));
+        }
+
+
+
+        // --- Betslip sorting
+
+        public static void SortBetslipListByDate(List<Betslip> _betslipList)
+        {
+            _betslipList.Sort((b1, b2) => b1.Date.CompareTo(b2.Date));
+        }
+    }
+}
