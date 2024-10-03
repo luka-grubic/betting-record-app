9a25c9cf184d9d66248f64c5cd45ca188299bd4f
Luka Grubić <luka.grubic.ck@gmail.com>
2024-10-03 15:52:18 +0200

    Init VS project

---
+++ BettingRecordApp/Common/CommonConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace BettingRecordApp
+{
+    public static class CommonConstant
+    {
+        public const string VERSION = "1.0.0";
+    }
+}
---
+++ BettingRecordApp/Forms/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace BettingRecordApp
+{
+    public partial class MainForm : Form
+    {
+        // --- Fields ---
+
+        // --- Init ---
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.Text = $"BettingRecordApp (v{CommonConstant.VERSION})";
+        }
+
+
+
+        // --- Actions ---
+
+        // --- Other
+    }
+}
